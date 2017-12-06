70e9d0f9d1fca1c4a1b6fddceab5033e69b05b79
Brooke Anderson <brooke.anderson@colostate.edu>
2017-12-05 21:11:45 -0700

    Copy efficacy clean-up function to revised helper

--- Shiny_App/helper_revised.R
+++ Shiny_App/helper_revised.R
@@ -2,6 +2,54 @@
 
 library(dplyr)
 
+#### efficacy_function cleans raw efficacy data in Shiny app
+# Function Title: Cleaning Efficacy Dataframe
+
+# This function uses the file input from the fileInput widget for "efficacy" as the argument. 
+# The dataframe explores lung and spleen efficacies by drug, days of treatment, and dosage. The function 
+# cleans the plasma dataframe by first removing columns that are repeating (i.e., units) and putting
+# the efficacy values into a log value for easier comprehension. Further, the dosage and days_treatment columns
+# were cleaned by changing the factor names in order to compare by dosage and include controls in this analysis.
+
+library(dplyr)
+
+efficacy_function <- function(efficacy_df){
+  efficacy_clean <- efficacy_df %>% 
+    select(Protocol_Animal, Compound, Group, Drug_Dose, Days_Treatment,
+           Treatment_Interval,Elung,Espleen) %>% 
+    rename(lung_efficacy = Elung,
+           spleen_efficacy = Espleen,
+           dosage = Drug_Dose,
+           days_treatment = Days_Treatment,
+           dose_interval = Treatment_Interval, 
+           drug = Compound) %>%
+    mutate(lung_efficacy = as.numeric(lung_efficacy)) %>% 
+    mutate(spleen_efficacy = as.numeric(spleen_efficacy)) %>%
+    mutate(dose_interval = as.factor(dose_interval)) %>%
+    mutate(days_treatment = as.factor(days_treatment)) %>% 
+    group_by(Protocol_Animal, drug, Group, dosage, days_treatment, dose_interval) %>% 
+    summarize(lung_efficacy_log = log10(lung_efficacy),
+              spleen_efficacy_log = log10(spleen_efficacy))
+  
+  levels(efficacy_clean$dose_interval)[levels(efficacy_clean$dose_interval)=="Pre Rx 9 week"] <- "_Baseline"
+  levels(efficacy_clean$dose_interval)[levels(efficacy_clean$dose_interval)=="M-F"] <- "_QD"
+  levels(efficacy_clean$dose_interval)[levels(efficacy_clean$dose_interval)=="4 wk"] <- "20_Control"
+  levels(efficacy_clean$dose_interval)[levels(efficacy_clean$dose_interval)=="8 wk"] <- "40_Control"
+  levels(efficacy_clean$drug)[levels(efficacy_clean$drug)==""] <- "Baseline"
+  
+  
+  efficacy_clean <- efficacy_clean %>% 
+    unite(days_dose, days_treatment, dose_interval, sep = "") %>% 
+    separate(days_dose, c("days", "dose"), sep = "_") %>% 
+    rename("days_treatment" = days,
+           "dose_interval" = dose) %>% 
+    mutate(days_treatment = as.numeric(days_treatment))
+  
+  return(efficacy_clean)
+}
+
+
+
 ##### Clean the tissue laser data into a tidy format
 
 # 
