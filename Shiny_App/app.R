27de28f42891fa52f89ddb6b88d975a3b6e28d70
Katie <katie.key@colostate.edu>
2017-12-12 08:22:58 -0700

    linear model function works!

--- Shiny_App/app.R
+++ Shiny_App/app.R
@@ -1415,77 +1415,78 @@ server <- function(input, output) {
                                                  tissue_std_pk_summarized,
                                                  in_vitro_clean)
     
-      
       if(input$linear_variable == "ELU") {
-        
-        function_data_ELU <- efficacy_summary_file %>% 
-          dplyr::filter(level == input$linear_level) %>% 
-          gather(key = independent_var, value = indep_measure, -drug, -dosage, -dose_int, -level, -ELU, -ESP, na.rm = TRUE)
-          
-          function_data_ELU <- function_data_ELU %>% 
-          dplyr::select(drug, dosage, dose_int, level, ELU, indep_measure, independent_var) 
-        
-          model_function_ELU <- function() {
-             lm(function_data_ELU$ELU ~ scale(function_data_ELU$indep_measure))
-          }
-        
-        estimate_results_ELU <- function_data_ELU %>% 
-          group_by(independent_var, dose_int) %>% 
-          nest() %>% 
-          mutate(mod_results = purrr::map(data, model_function_ELU())) %>% 
-          mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
-          select(independent_var, dose_int, mod_results, mod_coefs) %>% 
-          unnest(mod_coefs) %>% 
-          filter(term == "scale(indep_measure)")
-        
-        coef_plot_ELU <- estimate_results_ELU %>%
-          mutate(independent_var = forcats::fct_reorder(independent_var, estimate, fun = max)) %>%
-          rename(Dose_Interval = dose_int) %>% 
-          ggplot(aes(x = estimate, y = independent_var, color = Dose_Interval)) +
-          geom_point(aes(size = 1 / std.error)) +
-          scale_size_continuous(guide = FALSE) +
-          theme_few() + 
-          ggtitle(label = "Linear model coefficients as function of independent variables, \n by drug dose and model uncertainty", subtitle = "smaller points have more uncertainty than larger points") +
-          geom_vline(xintercept = 0, color = "cornflower blue") 
-        
-        return(coef_plot_ELU)
+      
+      function_data <- efficacy_summary_file %>% 
+        filter(level == input$linear_level) %>% 
+        gather(key = independent_var, value = indep_measure, -drug, -dosage, 
+               -dose_int, -level, -ELU, -ESP, na.rm = TRUE) %>% 
+        select(drug, dosage, dose_int, level, ELU, indep_measure, independent_var) 
+      
+      function_data$vect <- function_data$ELU
+      
+      model_function <- function(data) {
+        model_results <- lm(vect ~ scale(indep_measure), data = data)
       }
       
-      if(input$linear_variable == "ESP") {
-        
-        function_data_ESP <- efficacy_summary_file %>%
-          dplyr::filter(level == input$linear_level) %>% 
-          gather(key = independent_var, value = indep_measure, -drug, -dosage, -dose_int, -level, -ELU, -ESP, na.rm = TRUE)
-          
-          function_data_ESP <- function_data_ESP %>% 
-          dplyr::select(drug, dosage, dose_int, level, ESP, indep_measure, independent_var)
-          
-          model_function_ESP <- function() {
-            lm(function_data_ESP$ESP ~ scale(function_data_ESP$indep_measure))
-          }
-        
-        estimate_results_ESP <- function_data_ESP %>% 
-          group_by(independent_var, dose_int) %>% 
-          nest() %>% 
-          mutate(mod_results = purrr::map(data, model_function_ESP())) %>% 
-          mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
-          select(independent_var, dose_int, mod_results, mod_coefs) %>% 
-          unnest(mod_coefs) %>% 
-          filter(term == "scale(indep_measure)")
-        
-        coef_plot_ESP <- estimate_results_ESP %>%
-          mutate(independent_var = forcats::fct_reorder(independent_var, estimate, fun = max)) %>%
-          rename(Dose_Interval = dose_int) %>% 
-          ggplot(aes(x = estimate, y = independent_var, color = Dose_Interval)) +
-          geom_point(aes(size = 1 / std.error)) +
-          scale_size_continuous(guide = FALSE) +
-          theme_few() + 
-          ggtitle(label = "Linear model coefficients as function of independent variables, \n by drug dose and model uncertainty", subtitle = "smaller points have more uncertainty than larger points") +
-          geom_vline(xintercept = 0, color = "cornflower blue") 
-        
-        return(coef_plot_ESP)
-        }
+      estimate_results <- function_data %>% 
+        group_by(independent_var, dose_int) %>% 
+        nest() %>% 
+        mutate(mod_results = purrr::map(data, model_function)) %>% 
+        mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
+        select(independent_var, dose_int, mod_results, mod_coefs) %>% 
+        unnest(mod_coefs) %>% 
+        filter(term == "scale(indep_measure)")
       
+      coef_plot <- estimate_results %>%
+        mutate(independent_var = forcats::fct_reorder(independent_var, estimate, fun = max)) %>%
+        rename(Dose_Interval = dose_int) %>% 
+        ggplot(aes(x = estimate, y = independent_var, color = Dose_Interval)) +
+        geom_point(aes(size = 1 / std.error)) +
+        scale_size_continuous(guide = FALSE) +
+        theme_few() + 
+        ggtitle(label = "Linear model coefficients as function of independent variables, \n by drug dose and model uncertainty", subtitle = "smaller points have more uncertainty than larger points") +
+        geom_vline(xintercept = 0, color = "cornflower blue") 
+      
+      return(coef_plot)
+    }
+      
+      if(input$linear_variable == "ESP") {
+      
+      function_data <- efficacy_summary_file %>% 
+        filter(level == input$linear_level) %>% 
+        gather(key = independent_var, value = indep_measure, -drug, -dosage, 
+               -dose_int, -level, -ELU, -ESP, na.rm = TRUE) %>% 
+        select(drug, dosage, dose_int, level, ESP, indep_measure, independent_var) 
+      
+      function_data$vect <- function_data$ESP
+      
+      model_function <- function(data) {
+        model_results <- lm(vect ~ scale(indep_measure), data = data)
+      }
+      
+      estimate_results <- function_data %>% 
+        group_by(independent_var, dose_int) %>% 
+        nest() %>% 
+        mutate(mod_results = purrr::map(data, model_function)) %>% 
+        mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
+        select(independent_var, dose_int, mod_results, mod_coefs) %>% 
+        unnest(mod_coefs) %>% 
+        filter(term == "scale(indep_measure)")
+      
+      coef_plot <- estimate_results %>%
+        mutate(independent_var = forcats::fct_reorder(independent_var, estimate, fun = max)) %>%
+        rename(Dose_Interval = dose_int) %>% 
+        ggplot(aes(x = estimate, y = independent_var, color = Dose_Interval)) +
+        geom_point(aes(size = 1 / std.error)) +
+        scale_size_continuous(guide = FALSE) +
+        theme_few() + 
+        ggtitle(label = "Linear model coefficients as function of independent variables, \n by drug dose and model uncertainty", subtitle = "smaller points have more uncertainty than larger points") +
+        geom_vline(xintercept = 0, color = "cornflower blue") 
+      
+      return(coef_plot)
+      
+      }
     })
     
 ######### not sure if we need this plot since it uses clean_2_combined.csv
--- Shiny_App/trying_functions.R
+++ Shiny_App/trying_functions.R
@@ -55,27 +55,31 @@ library(stats)
   
  efficacy_summary <- read_csv("~/Documents/Colorado State/R_Programming_Directory/input_output_shiny_group/CSV_Files/efficacy_summary.csv")
   
-    function_data_ELU <- efficacy_summary %>% 
-      dplyr::filter(level == "Cmax") %>% 
-      gather(key = independent_var, value = indep_measure, -drug, -dosage, -dose_int, 
-             -level, -ELU, -ESP, na.rm = TRUE)
+ 
     
-    function_data_ELU <- function_data_ELU %>% 
-      dplyr::select(drug, dosage, dose_int, level, ELU, indep_measure, independent_var) 
-
-      model_results <- lm(function_data_ELU$ELU ~ scale(function_data_ELU$indep_measure))
+    function_data <- efficacy_summary %>% 
+      filter(level == "Cmax") %>% 
+      gather(key = independent_var, value = indep_measure, -drug, -dosage, 
+             -dose_int, -level, -ELU, -ESP, na.rm = TRUE) %>% 
+      select(drug, dosage, dose_int, level, ELU, indep_measure, independent_var) 
     
-    estimate_results_ELU <- function_data_ELU %>% 
+    if(dep_var=="ELU") {function_data$vect <- function_data$ELU}
+    if(dep_var=="ESP") {function_data$vect <- function_data$ESP}
+    
+    model_function <- function(data) {
+      model_results <- lm(vect ~ scale(indep_measure), data = data)
+    }
+    
+    estimate_results <- function_data %>% 
       group_by(independent_var, dose_int) %>% 
-      nest() %>%
-      dplyr::mutate(mod_results = purrr::map(data, model_results)) %>% 
-      dplyr::mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
-      dplyr::select(independent_var, dose_int, mod_results, mod_coefs) %>% 
+      nest() %>% 
+      mutate(mod_results = purrr::map(data, model_function)) %>% 
+      mutate(mod_coefs = purrr::map(mod_results, broom::tidy)) %>% 
+      select(independent_var, dose_int, mod_results, mod_coefs) %>% 
       unnest(mod_coefs) %>% 
       filter(term == "scale(indep_measure)")
-    ####issue is here in code - I think with the map function
     
-    coef_plot_ELU <- estimate_results_ELU %>%
+    coef_plot <- estimate_results %>%
       mutate(independent_var = forcats::fct_reorder(independent_var, estimate, fun = max)) %>%
       rename(Dose_Interval = dose_int) %>% 
       ggplot(aes(x = estimate, y = independent_var, color = Dose_Interval)) +
@@ -85,65 +89,5 @@ library(stats)
       ggtitle(label = "Linear model coefficients as function of independent variables, \n by drug dose and model uncertainty", subtitle = "smaller points have more uncertainty than larger points") +
       geom_vline(xintercept = 0, color = "cornflower blue") 
     
-    return(coef_plot_ELU)
+    coef_plot
     
-    
-#######
-    
-    dataz <- efficacy_summary %>% 
-      na.omit(efficacy_summary) %>% 
-      dplyr::mutate(as.numeric(as.integer(dosage))) %>% 
-      dplyr::select_if(is.numeric) %>%
-      filter(dosage == 50)
-    
-    response <- dataz %>% 
-      dplyr::select(input$variable_lasso)
-    
-    predictors <- dataz %>%
-      dplyr::select(c("PLA", "ULU", "RIM", "OCS", "ICS", "SLU", "SLE", "cLogP", "huPPB", 
-                      "muPPB", "MIC_Erdman", 'MICserumErd', "MIC_Rv", "Caseum_binding", "MacUptake"))
-    
-    y <- as.numeric(unlist(response))
-    x <- as.matrix(predictors)
-    
-    fit =  glmnet(x, y)
-    
-    coeff_50 <- coef(fit,s=0.1)
-    coeff_50 <- as.data.frame(as.matrix(coeff_50)) %>% 
-      rownames_to_column() 
-    colnames(coeff_50) <- c("predictor", "coeff")
-    
-    coeff_50 <- coeff_50 %>% 
-      dplyr::filter(coeff_50 > 0)
-    coeff_50
-    
-#####
-    
-    dataz_1 <- efficacy_summary %>% 
-      na.omit(efficacy_summary) %>% 
-      dplyr::mutate(dosage = as.numeric(as.integer(dosage))) %>% 
-      dplyr::select_if(is.numeric) %>%
-      filter(dosage == 100)
-    
-    response_1 <- dataz_1 %>% 
-      dplyr::select("ELU")
-    
-    predictors_1 <- dataz_1 %>%
-      dplyr::select(c("PLA", "ULU", "RIM", "OCS", "ICS", "SLU", "SLE", "cLogP", "huPPB", 
-                      "muPPB", "MIC_Erdman", 'MICserumErd', "MIC_Rv", "Caseum_binding", "MacUptake"))
-    
-    y <- as.numeric(unlist(response_1))
-    x <- as.matrix(predictors_1)
-    
-    fit =  glmnet(x, y)
-    
-    coeff <- coef(fit,s=0.1)
-    coeff <- as.data.frame(as.matrix(coeff)) %>% 
-      rownames_to_column() 
-    colnames(coeff) <- c("predictor", "coeff")
-    
-    coeff <- coeff %>% 
-      dplyr::filter(coeff > 0)
-   coeff
-    
-  
